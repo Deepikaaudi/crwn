9376be73d9bdaf630f5e13bafc9ec3c7bcb69214
Deepikaaudi <deepika.audi@gmail.com>
2020-05-12 12:29:42 +0530

    Added Homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HomePage }from './homePage.component.jsx';
+
+
+function App() {
+  return (
+    <div >
+<HomePage/>
+    </div>
+  );
+}
+
+export default App;
